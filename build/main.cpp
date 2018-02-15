11402b133599dd8f994b7937eb9f313bdd210231
AndySrb <36510898+AndySrb@users.noreply.github.com>
2018-02-15 17:07:38 +0100

    Add files via upload

---
+++ build/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <fstream>
+#include "base.h"
+#include <memory>
+int main()
+{
+	
+	Table table;
+	table.start();
+	table.output_code();
+//      table.print();
+//	std::shared_ptr<Table> table = std::make_shared<Table>();
+//	table->start();	
+//	table->print();
+
+}
+
