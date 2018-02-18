80305c21c0398bbdb74c5ac78b6a4291aff91a83
AndySrb <36510898+AndySrb@users.noreply.github.com>
2018-02-18 13:31:14 +0000

    Add files via upload

--- build/main.cpp
+++ build/main.cpp
@@ -3,14 +3,19 @@
 #include <string>
 #include <fstream>
 #include "base.h"
+#include "base.cpp"
+#include "convert.h"
+#include "convert.cpp"
 #include <memory>
 int main()
 {
 	
 	Table table;
 	table.start();
-	table.output_code();
-//      table.print();
+	table.print();
+	Convert convert(table);
+	convert.toTex(table);
+	return 0;
 //	std::shared_ptr<Table> table = std::make_shared<Table>();
 //	table->start();	
 //	table->print();
